edf2f1c6e5f97925559e0afa51331dca4cf1aab1
francisco123neto <91096585+francisco123neto@users.noreply.github.com>
2022-04-08 09:59:35 -0300

    att 0.02

---
+++ api/migrations/20220408121816-create-pessoas.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Pessoas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING
+      },
+      cpf: {
+        type: Sequelize.STRING
+      },
+      nascimento: {
+        type: Sequelize.STRING
+      },
+      tipoSanguineo: {
+        type: Sequelize.STRING
+      },
+      sexo: {
+        type: Sequelize.STRING
+      },
+      statusPessoa: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Pessoas');
+  }
+};
