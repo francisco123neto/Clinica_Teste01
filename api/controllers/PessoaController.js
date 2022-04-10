47dd74d6c71e0fb0a933cf4efa9a29f1eedfa384
francisco123neto <91096585+francisco123neto@users.noreply.github.com>
2022-04-10 15:21:26 -0300

    att 1.06
    - Novas funções e rotas

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -19,12 +19,51 @@ class PessoaController {
                     id: Number(id)
                 }
             })
-            return res.status(200), json(umaPessoa)
+            return res.status(200).json(umaPessoa)
         } catch (error) {
             return res.status(500).json(error.message)
         }
 
     }
+
+    static async criaPessoa(req, res) {
+        const novaPessoa = req.body
+        try {
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
+            return res.status(200).json(novaPessoaCriada)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+
+    static async atualizaPessoa(req, res) {
+        const { id } = req.params
+        const novasInfos = req.body
+        try {
+            await database.Pessoas.update(novasInfos, {
+                where: { id: Number(id) }
+            })
+            const pessoaAtualizada = await database.Pessoas.findOne({
+                where: { id: Number(id) }
+            })
+            return res.status(200).json(pessoaAtualizada)
+
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    /*static async deletaPessoa(req, res) {
+        con
+        try {
+
+
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }*/
+
 }
 
 module.exports = PessoaController
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -5,5 +5,8 @@ const PessoaController = require('../controllers/PessoaController')
 const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+router.post('/pessoas', PessoaController.criaPessoa)
+router.put('/pessoas/:id', PessoaController.atualizaPessoa)
 
 module.exports = router
