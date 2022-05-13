ae80295eea2db5725830f2e6b77dd8e8a2153903
francisco123neto <91096585+francisco123neto@users.noreply.github.com>
2022-05-13 10:10:15 -0300

    att 2.1

---
+++ api/components/app.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View, Image, TextInput, TouchableOpacity } from 'react-native'
+
+export default class App extends Component {
+    render() {
+        return (
+            <View style={StyleSheet.container}>
+
+                <TextInput
+                style={StyleSheet.input}
+                placeholder="Digite seu email"
+                />
+
+                <TextInput
+                style
+                
+
+
+
+            </View>
+        )
+}
